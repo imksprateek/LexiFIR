c7983aed158c68200721a0aaeddb741835f66f4c
imksprateek <ksprateek2004@gmail.com>
2024-11-15 18:00:11 +0530

    Configured Spring Security and JWT auth

---
+++ backend-server/src/main/java/studio/ksprateek/service/config/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package studio.ksprateek.service.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import studio.ksprateek.service.util.jwtutils.JwtAuthenticationEntryPoint;
+import studio.ksprateek.service.util.jwtutils.JwtFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private JwtFilter filter;
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.requestMatchers("/swagger-ui/**","/swagger-resources/*",
+                                "/v3/api-docs/**", "/login").permitAll()
+                        .anyRequest().authenticated())
+                // Send a 401 error response if user is not authentic.
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint))
+                // no session management
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // filter the request and add authentication token
+                .addFilterBefore(filter,  UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    AuthenticationManager customAuthenticationManager() {
+        return authentication -> new UsernamePasswordAuthenticationToken("randomuser123","password");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("My REST API")
+                        .description("Some custom description of API.")
+                        .version("1.0").contact(new Contact().name("Sallo Szrajbman")
+                                .email( "www.baeldung.com").url("salloszraj@gmail.com"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
---
+++ backend-server/src/main/java/studio/ksprateek/service/util/jwtutils/JwtController.java
@@ -0,0 +1,47 @@
+package studio.ksprateek.service.util.jwtutils;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import studio.ksprateek.service.util.jwtutils.models.JwtResponseModel;
+import studio.ksprateek.service.util.jwtutils.models.JwtRequestModel;
+
+@RestController
+@CrossOrigin
+@Tag(name = "JWT Controller")
+public class JwtController {
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenManager tokenManager;
+
+    // Get a JWT Token once user is authenticated, otherwise throw BadCredentialsException
+    @PostMapping("/login")
+    @Operation(summary = "To get JWT Token")
+    public ResponseEntity<JwtResponseModel> createToken(@RequestBody JwtRequestModel
+                                                                request) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        final String jwtToken = tokenManager.generateJwtToken(userDetails);
+        return ResponseEntity.ok(new JwtResponseModel(jwtToken));
+    }
+}
