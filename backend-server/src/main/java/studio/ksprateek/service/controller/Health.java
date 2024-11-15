87929baec029c3d8f4d17812d47c604a8e977982
imksprateek <ksprateek2004@gmail.com>
2024-11-15 18:00:11 +0530

    Configured Spring Security and JWT auth

--- backend-server/src/main/java/studio/ksprateek/service/controller/Health.java
+++ backend-server/src/main/java/studio/ksprateek/service/controller/Health.java
@@ -1,13 +1,18 @@
 package studio.ksprateek.service.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/health")
+@RequestMapping("/api/v3/health")
+@Tag(name = "Health Check")
 public class Health {
+
+    @Operation(summary = "To check server health")
     @GetMapping("")
     public ResponseEntity<?> healthCheck(){
         return ResponseEntity.ok("Up and running");
