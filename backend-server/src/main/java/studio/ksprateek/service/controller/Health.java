aea57d7181fe69e48972914c871d1ee1863f19d2
imksprateek <ksprateek2004@gmail.com>
2024-11-17 19:32:49 +0530

    Implemented role based authorization

--- backend-server/src/main/java/studio/ksprateek/service/controller/Health.java
+++ backend-server/src/main/java/studio/ksprateek/service/controller/Health.java
@@ -3,12 +3,14 @@ package studio.ksprateek.service.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/v3/health")
+@RequestMapping("/api/health")
 @Tag(name = "Health Check")
 public class Health {
 
