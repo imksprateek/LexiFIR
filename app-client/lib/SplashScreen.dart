166b02c2c13af963a316c9ff3e03e3e986d7877a
Kedarbhagat <bhagatkedar123@gmail.com>
2024-11-22 18:05:06 +0530

    File Structure

--- app-client/lib/SplashScreen.dart
+++ app-client/lib/SplashScreen.dart
@@ -1,6 +1,7 @@
-import 'dart:async'; // Required for Timer
-
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+import 'package:app_client/auth/LoginScreen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,6 +14,32 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+
+    Future<void> _checkToken() async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('jwt_token'); // Retrieve the token
+
+      // Simulate a delay for the splash screen (optional)
+      await Future.delayed(Duration(seconds: 2));
+
+      // Navigate based on token availability
+      if (token != null && token.isNotEmpty) {
+        // Token exists, navigate to home screen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      } else {
+        // Token doesn't exist, navigate to login screen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Loginscreen()),
+        );
+      }
+    }
+
+
+
 
     // Navigate to the next screen after a delay
     Timer(Duration(seconds: 4), () {
