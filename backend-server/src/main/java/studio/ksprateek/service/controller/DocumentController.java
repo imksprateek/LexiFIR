8c9c45b98d1e85e3f4629513d916e13974fa6149
imksprateek <ksprateek2004@gmail.com>
2024-11-23 22:25:10 +0530

    Added S3 file upload feature

--- backend-server/src/main/java/studio/ksprateek/service/controller/DocumentController.java
+++ backend-server/src/main/java/studio/ksprateek/service/controller/DocumentController.java
@@ -2,10 +2,20 @@ package studio.ksprateek.service.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import studio.ksprateek.service.entity.FIR;
 import studio.ksprateek.service.entity.UploadedDocument;
+import studio.ksprateek.service.entity.User;
+import studio.ksprateek.service.payload.responses.FileUploadResponse;
+import studio.ksprateek.service.repository.UserRepository;
 import studio.ksprateek.service.service.document.DocumentService;
 import studio.ksprateek.service.service.fir.FIRService;
 
@@ -13,7 +23,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/firs/{firId}/documents")
+@RequestMapping(path = "/api/firs/{firId}/documents")
 @Tag(name = "4. Document files", description = "Operations for managing user uploaded documents")
 public class DocumentController {
 
@@ -21,20 +31,35 @@ public class DocumentController {
     private DocumentService documentService;
     @Autowired
     private FIRService firService;
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping
-    @Operation(summary = "Upload a Document file")
-    public UploadedDocument uploadDocument(@PathVariable String firId, @RequestBody UploadedDocument document) {
-        Optional<FIR> fir = firService.getFIRById(firId);
-        if(fir.isPresent()){
-            document.setFirId(fir.get());
+@RequestMapping(
+        path = "/upload",
+        method = RequestMethod.POST,
+        consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<FileUploadResponse> uploadFile(@RequestPart("file") MultipartFile file) throws FileUploadException {
+        FileUploadResponse response = documentService.uploadFile(file, getCurrentUserId());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    private String getCurrentUserId() {
+        // Get the Authentication object from SecurityContextHolder
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        // If the principal is an instance of String, return it directly
+        if (principal instanceof String) {
+            return (String) principal;
         }
-        return documentService.uploadDocument(document);
+
+        Optional<User> user = userRepository.findByUsername(((UserDetails) principal).getUsername());
+
+        if(user.isPresent()) {
+            // Otherwise, assuming it's a UserDetails object, get the username (user ID)
+            return user.get().getId();
+        }
+        return "Error occurred while authorizing user";
     }
 
-    @GetMapping
-    @Operation(summary = "Get an uploaded Document file")
-    public List<UploadedDocument> getDocumentsByFIR(@PathVariable String firId) {
-        return documentService.getDocumentsByFIRId(firId);
-    }
+
 }
--- backend-server/src/main/java/studio/ksprateek/service/service/document/DocumentService.java
+++ backend-server/src/main/java/studio/ksprateek/service/service/document/DocumentService.java
@@ -1,39 +1,63 @@
 package studio.ksprateek.service.service.document;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import studio.ksprateek.service.entity.UploadedDocument;
-import studio.ksprateek.service.repository.DocumentRepository;
+import org.springframework.web.multipart.MultipartFile;
+import studio.ksprateek.service.payload.responses.FileUploadResponse;
 
-import java.util.List;
-import java.util.Optional;
-
+import java.io.IOException;
+import java.time.LocalDateTime;
 @Service
-public class DocumentService {
+@Slf4j
+public class DocumentService implements FileService {
 
-    @Autowired
-    private DocumentRepository documentRepository;
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
 
-    // Upload a document (Save to S3 and record in database)
-    @Transactional
-    public UploadedDocument uploadDocument(UploadedDocument document) {
-        return documentRepository.save(document);
+    @Value("${aws.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.s3.secretKey}")
+    private String secretKey;
+
+    private AmazonS3 s3Client;
+
+    @PostConstruct
+    private void initialize() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.US_EAST_1)
+                .build();
     }
 
-    // Get all documents for a given FIR
-    public List<UploadedDocument> getDocumentsByFIRId(String firId) {
-        return documentRepository.findByFirId(firId);
+    @Override
+    public FileUploadResponse uploadFile(MultipartFile multipartFile, String userID) throws FileUploadException {
+        FileUploadResponse fileUploadResponse = new FileUploadResponse();
+        String filePath = "";
+        try {
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(multipartFile.getContentType());
+            objectMetadata.setContentLength(multipartFile.getSize());
+            filePath = "Documents/" + userID+"/"+multipartFile.getOriginalFilename();
+            s3Client.putObject(bucketName, filePath, multipartFile.getInputStream(), objectMetadata);
+            fileUploadResponse.setFilePath(filePath);
+            fileUploadResponse.setDateTime(LocalDateTime.now());
+        } catch (IOException e) {
+            log.error("Error occurred ==> {}", e.getMessage());
+            throw new FileUploadException("Error occurred in file upload ==> "+e.getMessage());
+        }
+        return fileUploadResponse;
     }
 
-    // Get document by ID
-    public Optional<UploadedDocument> getDocumentById(String id) {
-        return documentRepository.findById(id);
-    }
 
-    // Delete a document
-    @Transactional
-    public void deleteDocument(String id) {
-        documentRepository.deleteById(id);
-    }
 }
