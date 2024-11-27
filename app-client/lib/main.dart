ffaadbed83600925b1109817585fab0bf6d2616c
venkatnarayanan.B <hybridedits2020@gmail.com>
2024-11-27 23:08:06 +0530

    speech to text package added

--- app-client/lib/main.dart
+++ app-client/lib/main.dart
@@ -1,6 +1,7 @@
 //import 'package:app_client/auth/LoginScreen.dart';
 import 'package:app_client/auth/Loginui.dart';
 import 'package:app_client/pages/HomeScreen.dart';
+import 'package:app_client/services/functions/speech_to_text.dart';
 import 'package:flutter/material.dart';
 import 'SplashScreen.dart';
 
@@ -14,8 +15,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-        home: SplashScreen());
+        debugShowCheckedModeBanner: false, home: SpeechToTextt());
   }
 }
-
