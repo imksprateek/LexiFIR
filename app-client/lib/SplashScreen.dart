f3f42fae1f070dd794eed02b0cf6884497378ee0
Kedarbhagat <bhagatkedar123@gmail.com>
2024-12-12 17:22:27 +0530

    hindi

--- app-client/lib/SplashScreen.dart
+++ app-client/lib/SplashScreen.dart
@@ -1,10 +1,10 @@
-import 'package:app_client/pages/HomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:video_player/video_player.dart';
 import 'dart:async';
 
 import 'auth/LoginUInew.dart';
+import 'pages/HomeScreen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -19,23 +19,25 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+
+    // Initialize video player
     _controller = VideoPlayerController.asset('assets/splash.mp4')
       ..initialize().then((_) {
-        setState(() {}); // Ensure the UI updates when the video is ready
-        _controller.play();
+        setState(() {
+          _controller.play();
+        }); // Update the UI when video is ready
       });
 
     _controller.setLooping(false);
-
-    _checkToken(); // Ensure this is called in initState
+    _checkToken(); // Check token after video initialization
   }
 
   Future<void> _checkToken() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('jwt_token'); // Retrieve the token
 
-    // Wait for the video to finish before navigating
-    await Future.delayed(_controller.value.duration + const Duration(seconds: 2)); // Combine video duration and optional delay
+    // Wait for video duration to finish + optional delay
+    await Future.delayed(_controller.value.duration + const Duration(seconds: 2));
 
     // Navigate based on token availability
     if (token != null && token.isNotEmpty) {
@@ -55,21 +57,21 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller.dispose(); // Dispose video player
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // Black background for the video
+      backgroundColor: Colors.black, // Black background for video
       body: Center(
         child: _controller.value.isInitialized
             ? AspectRatio(
           aspectRatio: _controller.value.aspectRatio,
           child: VideoPlayer(_controller),
         )
-            : const CircularProgressIndicator(), // Show a loading spinner while the video initializes
+            : const CircularProgressIndicator(), // Loading spinner during initialization
       ),
     );
   }
